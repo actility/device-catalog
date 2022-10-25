6fe79764acd76322017b0e52fd83d05836f616fc
Erwann WICART <erwann.wicart@actility.com>
2022-10-25 15:42:46 +0200

    [ADD] Add drivers for milesight.

---
+++ vendors/milesight-iot/drivers/vs121/index.js
@@ -0,0 +1,96 @@
+/**
+ * Payload Decoder for The Things Network
+ *
+ * Copyright 2020 Milesight IoT
+ *
+ * @product VS121
+ */
+function Decoder(bytes, port) {
+    var decoded = {};
+
+    for (i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+
+        // PROTOCOL VESION
+        if (channel_id === 0xff && channel_type === 0x01) {
+            decoded.protocol_version = bytes[i];
+            i += 1;
+        }
+        // SERIAL NUMBER
+        else if (channel_id === 0xff && channel_type === 0x08) {
+            decoded.sn = readString(bytes.slice(i, i + 6));
+            i += 6;
+        }
+        // HARDWARE VERSION
+        else if (channel_id === 0xff && channel_type === 0x09) {
+            decoded.hardware_version = readVersion(bytes.slice(i, i + 2));
+            i += 2;
+        }
+        // FIRMWARE VERSION
+        else if (channel_id === 0xff && channel_type === 0x1f) {
+            decoded.firmware_version = readVersion(bytes.slice(i, i + 4));
+            i += 4;
+        }
+        // PEOPLE COUNTER
+        else if (channel_id === 0x04 && channel_type === 0xc9) {
+            decoded.people_counter_all = bytes[i];
+            decoded.region_count = bytes[i + 1];
+            var region = readUInt16BE(bytes.slice(i + 2, i + 4));
+            for (var idx = 0; idx < decoded.region_count; idx++) {
+                var tmp = "region_" + (idx + 1);
+                decoded[tmp] = (region >> idx) & 1;
+            }
+            i += 4;
+        }
+        // PEOPLE IN/OUT
+        else if (channel_id ===0x05 && channel_type === 0xcc) {
+            decoded.in = readInt16LE(bytes.slice(i, i + 2));
+            decoded.out = readInt16LE(bytes.slice(i + 2, i + 4));
+            i += 4;
+        } 
+        // PEOPLE MAX
+        else if (channel_id ===0x06 && channel_type === 0xcd) {
+            decoded.people_max = bytes[i];
+            i += 1;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
+
+// bytes to number
+function readUInt16BE(bytes) {
+    var value = (bytes[0] << 8) + bytes[1];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+// bytes to version
+function readVersion(bytes) {
+    var temp = [];
+    for (var idx = 0; idx < bytes.length; idx++) {
+        temp.push((bytes[idx] & 0xff).toString(10));
+    }
+    return temp.join(".");
+}
+
+// bytes to string
+function readString(bytes) {
+    var temp = [];
+    for (var idx = 0; idx < bytes.length; idx++) {
+        temp.push(("0" + (bytes[idx] & 0xff).toString(16)).slice(-2));
+    }
+    return temp.join("");
+}
