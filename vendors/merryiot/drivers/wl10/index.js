717a1abcc7b02f3cb98b3130e3f39c6ea41cb101
Erwann WICART <erwann.wicart@actility.com>
2022-12-19 14:42:56 +0100

    Add extractPoints for wl10 merryiot.

--- vendors/merryiot/drivers/wl10/index.js
+++ vendors/merryiot/drivers/wl10/index.js
@@ -100,10 +100,10 @@ function extractPoints(input) {
         result.batteryVoltage = { unitId: "V", record: input.message.battery_volt };
     }
     if (input.message.temperature != undefined) {
-        result.temperature = { unitId: "Cel", record: input.message.temperature };
+        result.temperature = { unitId: "Cel", record: input.message.temperature, nature: "air" };
     }
     if (input.message.humi != undefined) {
-        result.humidity = { unitId: "%RH", record: input.message.humidity };
+        result.humidity = { unitId: "%RH", record: input.message.humidity, nature: "air" };
     }
     return result;
 }
