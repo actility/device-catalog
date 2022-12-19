0acf88af8fb90b223ce3b2f307cba1ffd4144edc
Erwann WICART <erwann.wicart@actility.com>
2022-12-19 14:30:26 +0100

    Add extractPoints for wl10 merryiot.

--- vendors/merryiot/drivers/wl10/index.js
+++ vendors/merryiot/drivers/wl10/index.js
@@ -94,6 +94,21 @@ function decodeUplink(input) {
     }
 }
 
+function extractPoints(input) {
+    var result = {};   
+    if (input.message.battery_volt != undefined) {
+        result.batteryVoltage = { unitId: "V", record: input.message.battery_volt };
+    }
+    if (input.message.temperature != undefined) {
+        result.temperature = { unitId: "Cel", record: input.message.temperature };
+    }
+    if (input.message.humi != undefined) {
+        result.humidity = { unitId: "%RH", record: input.message.humidity };
+    }
+    return result;
+}
+
+exports.extractPoints = extractPoints;
 exports.decodeUplink = decodeUplink;
 //////*BROWAN MerryIoT Water Leak Sensor --End!! *//////
 
