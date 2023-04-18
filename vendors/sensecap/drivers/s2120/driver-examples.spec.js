51a152282a9d32dd5fd94f0061bfeb4a140cb11e
Kway99 <kewei.li@seeed.cc>
2023-04-18 19:26:50 +0800

    Add S2120 and modify errors

---
+++ vendors/sensecap/drivers/s2120/driver-examples.spec.js
@@ -0,0 +1,26 @@
+const driver = require('./index.js')
+const examples = require('./examples.json')
+
+/*..............
+Test suites
+..............*/
+
+describe('Decode uplink', () => {
+    examples.forEach((example) => {
+        if (example.type === 'uplink') {
+            it(example.description, () => {
+                // Given
+                const input = {
+                    bytes: Buffer.from(example.input.bytes, 'hex'), fPort: example.input.fPort,
+                }
+
+                if (example.input.recvTime !== undefined) {
+                    input.recvTime = example.input.recvTime
+                }
+                // When
+                const result = driver.decodeUplink(input)
+                console.log(JSON.stringify(result))
+            })
+        }
+    })
+})
