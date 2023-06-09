b9319cbef1a5586c9eb11e9630a566de11c5ddb4
Simon Debray <simon.debray@actility.com>
2023-06-09 15:45:41 +0200

    Add tp-lsystemes with birdy-slim

---
+++ vendors/tpl-systemes/drivers/birdy-slim-iot/index.js
@@ -0,0 +1,76 @@
+function asciiToHexa(str){
+    var arr1 = [];
+    for (var n = 0, l = str.length; n < l; n ++) {
+        var hex = Number(str.charCodeAt(n)).toString(16);
+        arr1.push(hex);
+    }
+    return arr1.join('');
+}
+
+function hexaToAscii(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+
+/**
+ * @typedef {Object} DecodedUplink
+ * @property {Object} data - The open JavaScript object representing the decoded uplink payload when no errors occurred
+ * @property {string[]} errors - A list of error messages while decoding the uplink payload
+ * @property {string[]} warnings - A list of warning messages that do not prevent the driver from decoding the uplink payload
+ */
+
+/**
+ * Decode uplink
+ * @param {Object} input - An object provided by the IoT Flow framework
+ * @param {number[]} input.bytes - Array of bytes represented as numbers as it has been sent from the device
+ * @param {number} input.fPort - The Port Field on which the uplink has been sent
+ * @param {Date} input.recvTime - The uplink message time recorded by the LoRaWAN network server
+ * @returns {DecodedUplink} The decoded object
+ */
+function decodeUplink(input) {
+    let result = {
+        data: {},
+        errors: [],
+        warnings: []
+    };
+    const raw = Buffer.from(input.bytes);
+    result.data.message = hexaToAscii(raw);
+    return result;
+}
+
+/**
+ * @typedef {Object} EncodedDownlink
+ * @property {number[]} bytes - Array of bytes represented as numbers as it will be sent to the device
+ * @property {number} fPort - The Port Field on which the downlink must be sent
+ * @property {string[]} errors - A list of error messages while encoding the downlink object
+ * @property {string[]} warnings - A list of warning messages that do not prevent the driver from encoding the downlink object
+ */
+
+/**
+ * Downlink encode
+ * @param {Object} input - An object provided by the IoT Flow framework
+ * @param {Object} input.data - The higher-level object representing your downlink
+ * @returns {EncodedDownlink} The encoded object
+ */
+function encodeDownlink(input) {
+    let result = {
+        errors: [],
+        warnings: []
+    };
+    let raw = new Buffer(4);
+    let index = 0;
+
+    result.data = asciiToHexa(input.data);
+
+    result.bytes = Array.from(raw).slice(0,index);
+    result.fPort = 16;
+    return result;
+}
+
+
+exports.decodeUplink = decodeUplink;
+exports.encodeDownlink = encodeDownlink;
