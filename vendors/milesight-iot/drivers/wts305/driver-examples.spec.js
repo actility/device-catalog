8d493a6e01e36f9b224e3cbc886171967d64d009
tomHallActility <tom.hall@actility.com>
2023-06-15 13:31:22 +0200

    TXIF-2397 - Adding Milesight WTS305 Chirpstack decoder

---
+++ vendors/milesight-iot/drivers/wts305/driver-examples.spec.js
@@ -0,0 +1,30 @@
+const driver = require('./index.js')
+const examples = require('./examples.json')
+
+/*..............
+Test suites
+..............*/
+
+describe('Decode uplink', () => {
+  examples.forEach((example) => {
+    if (example.type === 'uplink') {
+      it(example.description, () => {
+        // Given
+        const input = {
+          bytes: Buffer.from(example.input.bytes, 'hex'), fPort: example.input.fPort,
+        }
+
+        if (example.input.recvTime !== undefined) {
+          input.recvTime = example.input.recvTime
+        }
+
+        // When
+        const result = driver.Decode(input.fPort, input.bytes)
+
+        // Then
+        const expected = example.output
+        expect(result).toStrictEqual(expected)
+      })
+    }
+  })
+})
