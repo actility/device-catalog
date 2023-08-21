821f8efd4a6e614c793e28509993923f073291f3
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-08-21 15:59:27 +0200

    fonctionnement atmo plus modif dans yaml de th

--- vendors/watteco/drivers/atm'o_v4/webpack.config.js
+++ vendors/watteco/drivers/atm'o_v4/webpack.config.js
@@ -1,54 +1,12 @@
 const path = require("path");
 
-module.exports={
+module.exports = {
     target: "node",
     mode: "production",
-    entry: {
-        "bundle.js":[path.resolve(__dirname,'../../codec/standard_minimized.js'),
-            path.resolve(__dirname,'../../codec/batch_minimized.js'),
-            path.resolve(__dirname,'../../codec/decode_minimized.js'),
-            path.resolve(__dirname,"../../captors/atm'o/atm'o.js"),
-        ]
-    },
+    entry: "./atm'o.js",
     output: {
-        filename: "index.js",
+        filename: "main.js",
         path: path.resolve(__dirname, "."),
-        library:{ name:"driver", type:"umd"}
-
-
-
+        library: "driver",
     },
-    optimization: {
-        minimizer: [
-            (compiler) => {
-                const TerserPlugin = require('terser-webpack-plugin');
-                new TerserPlugin({
-                    terserOptions: {
-                        compress: {
-                            dead_code: true,
-                            drop_console: false,
-                            drop_debugger: true,
-                            keep_classnames: false,
-                            keep_fargs: true,
-                            keep_fnames: false,
-                            keep_infinity: false
-                        },
-                        mangle: {
-                            eval: false,
-                            keep_classnames: false,
-                            keep_fnames: false,
-                            toplevel: false,
-                            safari10: false
-                        },
-                        module: false,
-                        sourceMap: false,
-                        output: {
-                            comments: 'some'
-                        }
-                    },
-                }).apply(compiler);
-            },
-        ],
-
-    },
-}
+};
