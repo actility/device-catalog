5d9ee4d7db86f7c7564f0e7319a286fa72a9681a
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-15 10:50:16 +0200

    flash'o

--- vendors/watteco/drivers/flash'o_v4/driver-examples.spec.js
+++ vendors/watteco/drivers/flash'o_v4/driver-examples.spec.js
@@ -1,5 +1,5 @@
 let examples = require("./examples.json");
-let driver = require("./index.js");
+let driver = require("./flash'o.js");
 /*..............
 Test suites
 ..............*/
---
+++ vendors/watteco/drivers/flash'o_v4/flash'o.js
@@ -0,0 +1,11 @@
+let watteco = require("../decode.js")
+
+let batch_param = [1, [{ taglbl: 0, resol: 1, sampletype: 10,lblname: "index", divide: 1},
+    { taglbl: 1, resol: 100, sampletype: 6,lblname: "battery_voltage", divide: 1000}]];
+
+let endpointCorresponder={}
+function decodeUplink(input) {
+    return result = watteco.watteco_decodeUplink(input,batch_param,endpointCorresponder);
+}
+exports.decodeUplink = decodeUplink;
+
