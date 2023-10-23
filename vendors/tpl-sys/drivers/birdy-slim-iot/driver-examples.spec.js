da66310a90a555cb10fa8d5b749bad302658d4bc
Mostafa Ibrahim <mostafa.ibrahim@actility.com>
2023-10-23 16:24:26 +0200

    Fixed encodeDownlink backward compatibility for tpl-sys birdy-slim-iot

--- vendors/tpl-sys/drivers/birdy-slim-iot/driver-examples.spec.js
+++ vendors/tpl-sys/drivers/birdy-slim-iot/driver-examples.spec.js
@@ -4,26 +4,60 @@ const examples = require("./examples.json");
 /*..............
 Test suites
 ..............*/
-
+// Convert hex string to bytes array
+function hexToBytes(hex) {
+    if(Array.isArray(hex)){
+        return hex;
+    }
+    let bytes = [];
+    for (c = 0; c < hex.length; c += 2) bytes.push(parseInt(hex.substr(c, 2), 16));
+    return bytes;
+}
 describe("Decode uplink", () => {
     examples.forEach((example) => {
         if (example.type === "uplink") {
             it(example.description, () => {
                 // Given
                 const input = {
-                    bytes: Buffer.from(example.input.bytes, "hex"),
+                    bytes: hexToBytes(example.input.bytes),
                     fPort: example.input.fPort,
                 };
+
                 if (example.input.recvTime !== undefined) {
                     input.recvTime = example.input.recvTime;
-
                 }
+
                 // When
                 const result = driver.decodeUplink(input);
 
                 // Then
                 const expected = example.output;
-                expect(result).toStrictEqual(expected);
+                expect(result).toEqual(expected);
+            });
+        }
+    });
+});
+
+describe("Decode downlink", () => {
+    examples.forEach((example) => {
+        if (example.type === "downlink-decode") {
+            it(example.description, () => {
+                // Given
+                const input = {
+                    bytes: hexToBytes(example.input.bytes),
+                    fPort: example.input.fPort,
+                };
+
+                if (example.input.recvTime !== undefined) {
+                    input.recvTime = example.input.recvTime;
+                }
+
+                // When
+                const result = driver.decodeDownlink(input);
+
+                // Then
+                const expected = example.output;
+                expect(result).toEqual(expected);
             });
         }
     });
@@ -35,9 +69,31 @@ describe("Encode downlink", () => {
             it(example.description, () => {
                 // When
                 const result = driver.encodeDownlink(example.input);
+
                 // Then
                 const expected = example.output;
-                expect(result).toStrictEqual(expected);
+                if(expected.bytes){
+                    expected.bytes = hexToBytes(expected.bytes);
+                }
+                expect(result).toEqual(expected);
+            });
+        }
+    });
+});
+
+describe("Backward compatibility - Encode downlink", () => {
+    examples.forEach((example) => {
+        if (example.type === "downlink-encode") {
+            it(example.description, () => {
+                // When
+                const result = driver.encodeDownlink(example.input.data);
+
+                // Then
+                const expected = example.output;
+                if(expected.bytes){
+                    expected.bytes = hexToBytes(expected.bytes);
+                }
+                expect(result).toEqual(expected);
             });
         }
     });
