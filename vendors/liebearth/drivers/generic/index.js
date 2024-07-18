9bbc6f55e7a078f8e63cb6ddcebacba9e1c2dd9b
saidialhaye10 <saidialhaye3@gmail.com>
2024-07-18 17:13:38 +0200

    driver reviewed  to create a generic onen and tests passed

--- vendors/liebearth/drivers/generic/index.js
+++ vendors/liebearth/drivers/generic/index.js
@@ -8,7 +8,7 @@ function decodeUplink(input) {
                                                      STATUS
           ____________________________________________________________________________________________________*/
        case 0x10:
-         channel = bytes[i + 1]; // get the channel
+         var channel = bytes[i + 1]; // get the channel
          switch (channel) {
            case 0x01: // battery
              data.battery = (bytes[i + 2] << 8) + bytes[i + 3]; // battery data of two bytes
@@ -32,7 +32,7 @@ function decodeUplink(input) {
                                                      MOISTURE
           ____________________________________________________________________________________________________*/
        case 0x20:
-         channel = bytes[i + 1]; // get the channel of one byte
+         var channel = bytes[i + 1]; // get the channel of one byte
          switch (channel) {
            case 0x01: // moisture_1 data  of two bytes
              data.moisture_1 = (bytes[i + 2] << 8) + bytes[i + 3];
@@ -120,79 +120,4 @@ function decodeUplink(input) {
    return output;
  }
  
- function encodeDownlink(input) {
-   var obj = input.data;
-   var bytes_array = [];
-   /*__________________________________________________________________________________________________
-                                            SOLENOID VALVE COMMAND
-    ____________________________________________________________________________________________________*/
-   if (keyExistInObject(obj, "command")) {
-     var command_array = obj.command;
-     for (var i = 0; i < command_array.length; i++) {
-       cmd = command_array[i];
-       if (cmd.open === true) {
-         // open
-         bytes_array = array_push(bytes_array, toHex(parseInt(0xf1)));
-         bytes_array = array_push(bytes_array, toHex(cmd.port));
- 
-         if (keyExistInObject(cmd, "duration")) {
-           // open for x duration
-           bytes_array = array_push(bytes_array, toHex(cmd.duration, 4));
-         } else {
-           // open permanently
-           bytes_array = array_push(bytes_array, toHex(0, 4));
-         }
-       } else {
-         // close
-         bytes_array = array_push(bytes_array, toHex(parseInt(0xf0)));
-         bytes_array = array_push(bytes_array, toHex(cmd.port));
-       }
-     }
-   }
- 
-   /*__________________________________________________________________________________________________
-                                                  PING
-   ____________________________________________________________________________________________________*/
-   if (keyExistInObject(obj, "ping")) {
-     bytes_array = array_push(bytes_array, toHex(parseInt(0x90)));
-     bytes_array = array_push(bytes_array, toHex(parseInt(0x00)));
-   }
-   return bytes_array;
- }
- 
- function array_push(original, toAdd) {
-   for (var i = 0; i < toAdd.length; i++) {
-     original[original.length] = toAdd[i];
-   }
-   return original;
- }
- 
- function keyExistInObject(object, key) {
-   return key in object;
- }
- 
- 
- function toHex(value, len) {
-   len = typeof len !== "undefined" ? len : 2;
-   hex_number = value.toString(16);
-   if (hex_number.length % 2 !== 0) {
-     hex_number = "0" + hex_number;
-   }
-   while (hex_number.length < len) {
-     hex_number = "0" + hex_number;
-   }
-   byte_array = [];
-   for (var i = 0; i < hex_number.length; i++) {
-     if (i % 2 === 0) {
-       byte_array.push(hex_number.substring(i, i + 2));
-     }
-   }
-   return byte_array;
- }
- 
- function decodeDownlink(input) {
-   return input;
- }
- 
  exports.decodeUplink = decodeUplink;
- exports.encodeDownlink = encodeDownlink;
