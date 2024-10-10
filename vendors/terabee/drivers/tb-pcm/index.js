bdf62fc1e68a89e8c0c9234361b8361a87f8739a
Tom Hall <tom.hall@actility.com>
2024-10-10 11:08:49 +0200

    Fixing profile deprecations on Terabee PC-L-XL

--- vendors/terabee/drivers/tb-pcm/index.js
+++ vendors/terabee/drivers/tb-pcm/index.js
@@ -334,3 +334,5 @@ function isKthBitSet(byte, k) {
     };
   
   }
+
+  exports.decodeUplink = decodeUplink;
--- vendors/terabee/drivers/tb-poc/index.js
+++ vendors/terabee/drivers/tb-poc/index.js
@@ -413,3 +413,5 @@ function isKthBitSet(byte, k) {
     };
   
   }
+  
+  exports.decodeUplink = decodeUplink;
