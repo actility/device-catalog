a9ef951769067d5c0f8c9478e6d96163e14343aa
wtc-peg <pe.goudet@watteco.com>
2025-01-17 18:19:45 +0100

    Fix: Sync sensor decoding for smartplug and hygrotyemp'o

--- vendors/watteco/drivers/hygrotemp'o/hygrotemp'o.js
+++ vendors/watteco/drivers/hygrotemp'o/hygrotemp'o.js
@@ -1,8 +1,8 @@
-let watteco = require("../decode.js")
+ let watteco = require("../decode.js")
 
-let batch_param = [2, [{ taglbl: 0, resol: 10, sampletype: 7, lblname: "Temperature", divide: 100 },
-    { taglbl: 1, resol: 100, sampletype: 6, lblname: "RelativeHumidity", divide: 100 },
-    { taglbl: 2, resol: 1, sampletype: 6, lblname: "BatteryLevel", divide: 1000 },
+let batch_param = [2, [{ taglbl: 0, resol: 10, sampletype: 7, lblname: "temperature", divide: 100 },
+    { taglbl: 1, resol: 100, sampletype: 6, lblname: "humidity", divide: 100 },
+    { taglbl: 2, resol: 1, sampletype: 6, lblname: "battery_voltage", divide: 1000 },
     { taglbl: 3, resol: 1, sampletype: 1, lblname: "violation_detection", divide: 1 }]];
 
 
@@ -14,4 +14,3 @@ function decodeUplink(input) {
 
 }
 exports.decodeUplink = decodeUplink;
-
--- vendors/watteco/drivers/smartplug/smartplug.js
+++ vendors/watteco/drivers/smartplug/smartplug.js
@@ -1,6 +1,6 @@
 let watteco = require("../decode.js")
 
-let batch_param=[]
+let batch_param = [1, [{ taglbl: 0, resol: 1, sampletype: 9, lblname: "ActiveEnergy", divide: 1 }]]
 let endpointCorresponder={}
 function decodeUplink(input) {
     return watteco.watteco_decodeUplink(input,batch_param,endpointCorresponder);
