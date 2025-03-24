1750e48b937ee81da66a377b07dbf735364fc3e0
Léna MABILLE <lena.mabille@actility.com>
2025-03-24 10:25:25 +0100

    fix comparaison <= in thermokon generic driver

--- vendors/thermokon/drivers/generic/index.js
+++ vendors/thermokon/drivers/generic/index.js
@@ -96,7 +96,7 @@ function Decode(fPort, bytes) {
 
         switch (lpp) {
             case LPP_PARSER:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.PARSER = u16_to_s16((data[i] << 8) | data[i + 1]);
                     i += 2;
                 }
@@ -108,7 +108,7 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_TEMP:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TEMP = u16_to_s16((data[i] << 8) | data[i + 1]) / 10;
                     i += 2;
                 }
@@ -120,37 +120,37 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_CO2:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.CO2 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_VOC:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.VOC = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_ATM_P:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.ATM_P = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_DP:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.DP = u16_to_s16((data[i] << 8) | data[i + 1]);
                     i += 2;
                 }
                 break;
             case LPP_FLOW:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.FLOW = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_VISIBLE_LIGHT:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.VISIBLE_LIGHT = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
@@ -170,7 +170,7 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_CONDENSATION:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.CONDENSATION_STATE = data[i] >>> 7;
                     decoded.CONDENSATION_RAW = u16_to_s16((data[i] << 8) | data[i + 1]) & 0x0FFF;
                     i += 2;
@@ -184,13 +184,13 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_SETPOINT:
-                if (i < data.length) {
+                if (i <= data.length) {
                     decoded.SETPOINT = data[i];
                     i++;
                 }
                 break;
             case LPP_VBAT_HI_RES:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.VBAT_HI_RES = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
@@ -210,19 +210,19 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_CONDENSATION_RAW:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.CONDENSATION_RAW = u16_to_s16((data[i] << 8) | data[i + 1]);
                     i += 2;
                 }
                 break;
             case LPP_DEV_KEY:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.DEV_KEY = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_CMD:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.CMD = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
@@ -234,31 +234,31 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_BAT_TYPE:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.BAT_TYPE = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_HEARTBEAT:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.HEARTBEAT = ((data[i] << 8) | data[i + 1]) / (1.0 / 60000);
                     i += 2;
                 }
                 break;
             case LPP_MEAS_INTERVAL:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.MEAS_INTERVAL = ((data[i] << 8) | data[i + 1]) / (1.0 / 1000);
                     i += 2;
                 }
                 break;
             case LPP_CNT_MEAS:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.CNT_MEAS = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_BIN_LATENCY:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.BIN_LATENCY = ((data[i] << 8) | data[i + 1]) / (1.0 / 1000);
                     i += 2;
                 }
@@ -270,43 +270,43 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_TLF_ONTIME:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TLF_ONTIME = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_TLF_INTERVAL_0:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TLF_INTERVAL_0 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_TLF_INTERVAL_1:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TLF_INTERVAL_1 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_TLF_INTERVAL_2:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TLF_INTERVAL_2 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_TLF_INTERVAL_3:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TLF_INTERVAL_3 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_TLF_INTERVAL_4:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TLF_INTERVAL_4 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_TLF_INTERVAL_5:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TLF_INTERVAL_5 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
@@ -318,49 +318,49 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_LED_ONTIME:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.LED_ONTIME = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_LED_INTERVAL_0:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.LED_INTERVAL_0 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_LED_INTERVAL_1:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.LED_INTERVAL_1 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_LED_INTERVAL_2:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.LED_INTERVAL_2 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_LED_INTERVAL_3:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.LED_INTERVAL_3 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_FORCED_UPLINK:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.FORCED_UPLINK = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_OCCU0ENABLE:
-                if (i < data.length) {
+                if (i <= data.length) {
                     decoded.OCCU0ENABLE = data[i];
                     i++;
                 }
                 break;
             case LPP_BUTTON:
-                if (i < data.length) {
+                if (i <= data.length) {
                     decoded.BUTTON_PRESSED = (data[i] >= 2) ? 1 : 0;
                     decoded.BUTTON_LAST_TYPE = ((data[i] & 0x01) >= 1) ? "Long Press" : "Short Press";
                     decoded.BUTTON_CNT = data[i] >> 1;
@@ -368,19 +368,19 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_TEMP_2:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TEMP_2 = u16_to_s16((data[i] << 8) | data[i + 1]) / 10;
                     i += 2;
                 }
                 break;
             case LPP_MOISTURE_2:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.MOISTURE_2 = (data[i] << 8) | data[i + 1];
                     i += 2;
                 }
                 break;
             case LPP_BUTTON_2:
-                if (i < data.length) {
+                if (i <= data.length) {
                     decoded.BUTTON_2_PRESSED = (data[i] >= 2) ? 1 : 0;
                     decoded.BUTTON_2_LAST_TYPE = ((data[i] & 0x01) >= 1) ? "Long Press" : "Short Press";
                     decoded.BUTTON_2_CNT = data[i] >> 1;
@@ -388,37 +388,37 @@ function Decode(fPort, bytes) {
                 }
                 break;
             case LPP_SETPOINT_2:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.SETPOINT_2 = ((data[i] << 8) | data[i + 1]) / 10;
                     i += 2;
                 }
                 break;
             case LPP_TEMP_3:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.TEMP_3 = u16_to_s16((data[i] << 8) | data[i + 1]) / 10;
                     i += 2;
                 }
                 break;
             case LPP_EPD_VALUE_EXT_OVERLOAD_0:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.EPD_VALUE_EXT_OVERLOAD_0 = u16_to_s16((data[i] << 8) | data[i + 1]);
                     i += 2;
                 }
                 break;
             case LPP_EPD_VALUE_EXT_OVERLOAD_1:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.EPD_VALUE_EXT_OVERLOAD_1 = u16_to_s16((data[i] << 8) | data[i + 1]);
                     i += 2;
                 }
                 break;
             case LPP_EPD_VALUE_EXT_OVERLOAD_2:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.EPD_VALUE_EXT_OVERLOAD_2 = u16_to_s16((data[i] << 8) | data[i + 1]);
                     i += 2;
                 }
                 break;
             case LPP_EPD_VALUE_EXT_OVERLOAD_3:
-                if (i + 1 < data.length) {
+                if (i + 1 <= data.length) {
                     decoded.EPD_VALUE_EXT_OVERLOAD_3 = u16_to_s16((data[i] << 8) | data[i + 1]);
                     i += 2;
                 }
