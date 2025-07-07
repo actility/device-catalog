7f26a02d18896b560be6c1d97cac8b0363ea9721
Léna MABILLE <lena.mabille@actility.com>
2025-07-07 17:43:38 +0200

    Fix at3 tests

--- iso-libraries/loraAllianceFnCall.js
+++ iso-libraries/loraAllianceFnCall.js
@@ -1,48 +1,50 @@
-function getDriverEngineResult(){
+function getDriverEngineResult() {
+    const drv =
+        (typeof driver !== 'undefined') ? driver :
+            (typeof exports !== 'undefined' && typeof exports.driver !== 'undefined') ? exports.driver :
+                (typeof exports !== 'undefined' && typeof exports.decodeUplink === 'function') ? exports :
+                    null;
+
     let fn = null;
 
-    if(typeof driver === 'undefined' || typeof driver?.decodeUplink !== 'function'){
-        if(operation === 'decodeUplink') {
-            if(exports?.decodeUplink !== null && typeof exports.decodeUplink === 'function') {
+    if (drv) {
+        if (operation === 'decodeUplink' && typeof drv.decodeUplink === 'function') {
+            fn = drv.decodeUplink(input);
+        } else if (operation === 'decodeDownlink' && typeof drv.decodeDownlink === 'function') {
+            fn = drv.decodeDownlink(input);
+        } else if (operation === 'encodeDownlink' && typeof drv.encodeDownlink === 'function') {
+            fn = drv.encodeDownlink(input);
+        } else {
+            throw new Error(`Unsupported operation ${operation}`);
+        }
+    } else {
+        if (operation === 'decodeUplink') {
+            if (typeof exports !== 'undefined' && typeof exports.decodeUplink === 'function') {
                 fn = exports.decodeUplink(input);
-            }
-            else if(decodeUplink !== null && typeof decodeUplink === 'function') {
+            } else if (typeof decodeUplink === 'function') {
                 fn = decodeUplink(input);
             }
-        }
-        
-        if(operation === 'encodeDownlink') {
-            if(exports?.encodeDownlink !== null && typeof exports.encodeDownlink === 'function') {
-                fn = exports.encodeDownlink(input);
+        } else if (operation === 'decodeDownlink') {
+            if (typeof exports !== 'undefined' && typeof exports.decodeDownlink === 'function') {
+                fn = exports.decodeDownlink(input);
+            } else if (typeof decodeDownlink === 'function') {
+                fn = decodeDownlink(input);
             }
-            else if(typeof encodeDownlink === 'function') {
+        } else if (operation === 'encodeDownlink') {
+            if (typeof exports !== 'undefined' && typeof exports.encodeDownlink === 'function') {
+                fn = exports.encodeDownlink(input);
+            } else if (typeof encodeDownlink === 'function') {
                 fn = encodeDownlink(input);
             }
         }
 
-        if(operation === 'decodeDownlink') {
-            if(exports?.decodeDownlink !== null && typeof exports.decodeDownlink === 'function') {
-                fn = exports.decodeDownlink(input);
-            }
-            else if(decodeDownlink !== null && typeof decodeDownlink === 'function') {
-                fn = decodeDownlink(input);
-            }
-        }
-    }
-    else {
-        if(operation === 'decodeUplink') {
-            fn = driver.decodeUplink(input);
-        }
-        else if(operation === 'decodeDownlink' && typeof driver.decodeDownlink === 'function') {
-            fn = driver.decodeDownlink(input);
-        }
-        else if(operation === 'encodeDownlink' && typeof driver.encodeDownlink === 'function') {
-            fn = driver.encodeDownlink(input);
+        if (fn === null) {
+            throw new Error(`Unsupported operation ${operation} on fallback context`);
         }
     }
 
-    if(typeof fn !== 'object') {
-        throw new Error(`Unsupported operation ${operation} on lora-alliance driver`);
+    if (typeof fn !== 'object') {
+        throw new Error(`Unexpected result type for operation ${operation}`);
     }
 
     return fn;
--- vendors/abeeway/drivers/asset-tracker-3/webpack.config.js
+++ vendors/abeeway/drivers/asset-tracker-3/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './'),
-     library: 'driver',
+    library: 'driver',
     filename: 'main.js',
     libraryTarget: 'umd',
     globalObject: 'this'
