20d4db3105d0e56bea6c07272233d69d038b7cfa
0Davgi0 <davgi.wifeo@gmail.com>
2025-09-01 11:43:44 +0200

    feat: Add downlink encoding and Units to decoding
     - feat: Add downlink encoding functionnality to all sensors (for BacNET purposes)
     - feat: Add brownout_number field to decoder and fix some labels in batch_param for lev'o and press'o
     - feat: Add units to decoder output
     - doc: Add watteco bacnet mapping csv file
     - fix: Correct bitwise operation precedence in TIC_Decode function for ICE Cluster
     - fix: Various other small fixes

--- vendors/watteco/drivers/monito_v1.1/monit'o.js
+++ vendors/watteco/drivers/monito_v1.1/monit'o.js
@@ -1,4 +1,5 @@
 let watteco = require("../../codec_v1.1/decode_uplink.js")
+let units = require("./units.auto.js")
 
 let batch_param = [3, [{taglbl: 0,resol: 0.02, sampletype: 12,lblname: "0-100_mV", divide: 1},
     { taglbl: 1, resol: 17, sampletype: 12,lblname: "0-70_V", divide: 1},
@@ -8,10 +9,11 @@ let batch_param = [3, [{taglbl: 0,resol: 0.02, sampletype: 12,lblname: "0-100_mV
 let endpointCorresponder = {
     analog:["0-100_mV","0-70_V"]
 }
-function decodeUplink(input,optBatchParams = null, optEndpointCorresponder = null) {
+function decodeUplink(input, optBatchParams = null, optEndpointCorresponder = null, optUnits = null) {
 	if (optBatchParams) { batch_param = optBatchParams;}
 	if (optEndpointCorresponder) { endpointCorresponder = optEndpointCorresponder;}
-	return watteco.watteco_decodeUplink(input,batch_param,endpointCorresponder);
+    if (optUnits) { units = { ...units, ...optUnits };}
+	return watteco.watteco_decodeUplink(input, batch_param, endpointCorresponder, units);
 }
 exports.decodeUplink = decodeUplink;
 
@@ -19,5 +21,30 @@ exports.decodeUplink = decodeUplink;
 // but keep former diver.decodeUplink format for retrocompatibility
 const globalObject = typeof globalThis !== 'undefined' ? globalThis : this;
 globalObject.decodeUplink = decodeUplink;
+
+// Add downlink encoder
+let encoder = require("../../codec/encode_downlink")
+
+// Define downlink frame templates below
+// Format: commandName: "hexadecimalPrefix<dataType:commandName>"
+// Example: sendMSOMode: "11050013005520<U8:sendMSOMode>" where:
+//   - "sendMSOMode" is used as the command identifier
+//   - "11050013005520" is the hex prefix of the frame
+//   - "U8" specifies an unsigned 8-bit integer data type
+//   - "sendMSOMode" is the parameter name that will be replaced with the actual value
+const dlFrames = {
+    
+}
+
+function encodeDownlink(input) {
+    return encoder.watteco_encodeDownlink({ dlFrames: dlFrames }, input);
+}
+exports.encodeDownlink = encodeDownlink;
+
+const encodePayload = encoder.encodePayload;
+exports.encodePayload = encodePayload;
+
+globalObject.encodeDownlink = encodeDownlink;
+globalObject.encodePayload = encodePayload;
 
 
--- vendors/watteco/drivers/smartplug_v1.1/smartplug.js
+++ vendors/watteco/drivers/smartplug_v1.1/smartplug.js
@@ -1,11 +1,13 @@
 let watteco = require("../../codec_v1.1/decode_uplink.js")
+let units = require("./units.auto.js")
 
 let batch_param = [1, [{ taglbl: 0, resol: 1, sampletype: 9, lblname: "ActiveEnergy", divide: 1 }]]
 let endpointCorresponder={}
-function decodeUplink(input,optBatchParams = null, optEndpointCorresponder = null) {
+function decodeUplink(input, optBatchParams = null, optEndpointCorresponder = null, optUnits = null) {
 	if (optBatchParams) { batch_param = optBatchParams;}
 	if (optEndpointCorresponder) { endpointCorresponder = optEndpointCorresponder;}
-	return watteco.watteco_decodeUplink(input,batch_param,endpointCorresponder);
+    if (optUnits) { units = { ...units, ...optUnits };}
+	return watteco.watteco_decodeUplink(input, batch_param, endpointCorresponder, units);
 }
 exports.decodeUplink = decodeUplink;
 
@@ -13,4 +15,29 @@ exports.decodeUplink = decodeUplink;
 // but keep former diver.decodeUplink format for retrocompatibility
 const globalObject = typeof globalThis !== 'undefined' ? globalThis : this;
 globalObject.decodeUplink = decodeUplink;
+
+// Add downlink encoder
+let encoder = require("../../codec/encode_downlink")
+
+// Define downlink frame templates below
+// Format: commandName: "hexadecimalPrefix<dataType:commandName>"
+// Example: sendMSOMode: "11050013005520<U8:sendMSOMode>" where:
+//   - "sendMSOMode" is used as the command identifier
+//   - "11050013005520" is the hex prefix of the frame
+//   - "U8" specifies an unsigned 8-bit integer data type
+//   - "sendMSOMode" is the parameter name that will be replaced with the actual value
+const dlFrames = {
+	sendOnOffState: "1150000600<U8:sendOnOffState>"
+}
+
+function encodeDownlink(input) {
+    return encoder.watteco_encodeDownlink({ dlFrames: dlFrames }, input);
+}
+exports.encodeDownlink = encodeDownlink;
+
+const encodePayload = encoder.encodePayload;
+exports.encodePayload = encodePayload;
+
+globalObject.encodeDownlink = encodeDownlink;
+globalObject.encodePayload = encodePayload;
 
