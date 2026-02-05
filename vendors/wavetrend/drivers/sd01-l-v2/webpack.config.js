6cd8f7424c144f4f233c8fcab76c51b0fcca2704
Léna MABILLE <lena.mabille@actility.com>
2026-02-05 09:36:21 +0100

    TXIF-3938 : Quick fix on PR to validate it

--- vendors/wavetrend/drivers/sd01-l-v2/webpack.config.js
+++ vendors/wavetrend/drivers/sd01-l-v2/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname),
     library: {
       type: 'commonjs'
     }
